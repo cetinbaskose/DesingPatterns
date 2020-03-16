067f7c060dacd78853d514627818c41472f9aa46
cetinbaskose <cetinbaskose@gmail.com>
2020-03-16 13:36:34 +0000

    first commit

---
+++ factorypattern/ShapeFactory.java
@@ -0,0 +1,22 @@
+package factorypattern;
+
+public class ShapeFactory {
+
+	public Shape getShape(String shapeType) {
+		if (shapeType == null) {
+			return null;
+		}
+
+		if (shapeType.equals("CIRCLE")) {
+			return new Circle();
+		} else if (shapeType.equals("RECTANGLE")) {
+			return new Rectangle();
+		} else if (shapeType.equals("SQUARE")) {
+			return new Square();
+		}
+
+		return null;
+
+	}
+
+}
