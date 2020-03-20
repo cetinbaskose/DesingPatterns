9ebd6d60781dd522ec8a692d6871da95bc4f4610
cetinbaskose <cetinbaskose@gmail.com>
2020-03-20 15:44:53 +0000

    added abstractfactory pattern

---
+++ abstractfactorypattern/RoundedRectangle.java
@@ -0,0 +1,10 @@
+package abstractfactorypattern;
+
+public class RoundedRectangle implements Shape {
+
+	@Override
+	public void draw()
+	{
+		System.out.println("Inside RoundedRectangle::draw() method.");
+	}
+}
---
+++ abstractfactorypattern/Shape.java
@@ -0,0 +1,6 @@
+package abstractfactorypattern;
+
+public interface Shape {
+
+	void draw();
+}
