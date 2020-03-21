0c3df1d083907d7747bcbddda4e9dc4c1f264dd8
cetinbaskose <cetinbaskose@gmail.com>
2020-03-21 18:26:18 +0000

    builderpattern added

---
+++ builderpattern/BuilderPatternDemo.java
@@ -0,0 +1,18 @@
+package builderpattern;
+
+public class BuilderPatternDemo {
+
+	public static void main(String[] args) {
+		MealBuilder mealBuilder = new MealBuilder();
+		Meal vegMeal = mealBuilder.prepareVegMeal();
+		System.out.println("Veg Meal");
+		vegMeal.showItems();
+		System.out.println("Total Cost: " + vegMeal.getCost());
+
+		Meal nonVegMeal = mealBuilder.prapareNonVegMeal();
+		System.out.println("Non-Veg Meal");
+		nonVegMeal.showItems();
+		System.out.println("Total Cost: " + nonVegMeal.getCost());
+	}
+
+}
